0b70c557c787d203cc29fca31c3ba4727db97ed9
JoanDoe11 <33231314+JoanDoe11@users.noreply.github.com>
2019-03-21 11:11:28 +0100

    create main class 4 testing mutex

---
+++ KIDS_course/src/mutex/Main.java
@@ -0,0 +1,16 @@
+package mutex;
+
+/**
+ * This is an exercise with the goal of understanding how mutex works.
+ *
+ * @author  JoanDoe11
+ * @version 1.0
+ * @since   2019-03-21
+ */
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
