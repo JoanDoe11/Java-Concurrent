370d4d538fbad8416342b124e94c7c4a18760fc0
JoanDoe11 <33231314+JoanDoe11@users.noreply.github.com>
2019-03-27 12:17:30 +0100

    add main test for concurrent pair

--- KIDS_course/src/atomic/Main.java
+++ KIDS_course/src/atomic/Main.java
@@ -1,14 +1,28 @@
 package atomic;
 
+import java.awt.*;
+
 public class Main {
 
-    private static final int THREAD_COUNT = 10;
+    private static final int THREAD_COUNT = 30;
     private static final int LIMIT = 3000;
+    private static final int WRITE_COUNT = 10000;
+
+
 
     public static void main(String[] args) {
+
+        pairTest();
+
+
+    }
+
+    private static void count(){
         ConcurrentBoundedCounter counter = new NaiveBoundedCounter(LIMIT);
         //ConcurrentBoundedCounter counter = new CASBoundedCounter(LIMIT);
+
         Thread[] threads = new Thread[THREAD_COUNT];
+
 
         for(int i=0; i<THREAD_COUNT; i++){
             threads[i] = new Thread(new CounterThread(counter));
@@ -27,5 +41,45 @@ public class Main {
         }
 
         System.out.println(counter.get());
+    }
+
+    private static void pairTest(){
+        //ConcurrentPair pair = new NaivePair();
+        ConcurrentPair pair = new CASConcurrentPair(WRITE_COUNT*THREAD_COUNT/2);
+
+        Thread[] threads = new Thread[THREAD_COUNT];
+
+
+        for (int i =0; i<THREAD_COUNT; i++){
+            if(i%2 == 0){
+                //writer threads
+                threads[i] = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for(int i=0;i<WRITE_COUNT; i+=2){
+                            pair.writePair(i,i+1);
+                        }
+                    }
+                });
+            } else {
+                //reader threads
+                threads[i] = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while(true){
+                            Point p = pair.readPair();
+                            if(p.x != p.y-1){
+                                System.out.println("Error");
+                            }
+                        }
+                    }
+                });
+                threads[i].setDaemon(true);
+            }
+        }
+
+        for(Thread t:threads){
+            t.start();
+        }
     }
 }
